43f64737b5094abf3e9f193ed1e64c277b9ed99d
Atomjoy <atomjoy.official@gmail.com>
2026-01-16 17:22:33 +0100

    DataTable search pagination

--- resources/js/pages/Payments/Index.vue
+++ resources/js/pages/Payments/Index.vue
@@ -176,40 +176,41 @@ const table = useVueTable({
 	<div class="full p-10">
 		<div class="rounded-md border mb-4 p-4" v-if="sorting">{{ sorting }} {{ rowSelection }} {{ columnFilters }}</div>
 
-		<div class="rounded-md border">
-			<div class="flex items-center p-4">
-				<Input
-					class="h-9 max-w-sm" placeholder="Filter emails..."
-					:model-value="table.getColumn('email')?.getFilterValue() as string"
-                	@update:model-value=" table.getColumn('email')?.setFilterValue($event)"
-				/>
+		<div class="flex items-center my-4">
+			<Input
+				class="h-9 max-w-sm" placeholder="Filter emails..."
+				:model-value="table.getColumn('email')?.getFilterValue() as string"
+				@update:model-value=" table.getColumn('email')?.setFilterValue($event)"
+			/>
 
-				<div v-for="filter in filter_toolbar" :key="filter.title">
-					<Filter :column="table.getColumn(filter.column)" :title="filter.title" :options="filter.data"></Filter>
-				</div>
-
-				<DropdownMenu>
-					<DropdownMenuTrigger as-child>
-						<Button variant="outline" class="ml-auto"> Columns <ChevronDown class="ml-2 h-4 w-4" /> </Button>
-					</DropdownMenuTrigger>
-
-					<DropdownMenuContent align="end">
-						<DropdownMenuCheckboxItem
-							v-for="column in table.getAllColumns().filter((column) => column.getCanHide())"
-							:key="column.id"
-							class="capitalize"
-							:modelValue="column.getIsVisible()"
-							@update:modelValue="
-								(value: any) => {
-									column.toggleVisibility(!!value);
-								}
-							"
-						>
-							{{ column.id }}
-						</DropdownMenuCheckboxItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
+			<div v-for="filter in filter_toolbar" :key="filter.title">
+				<Filter :column="table.getColumn(filter.column)" :title="filter.title" :options="filter.data"></Filter>
 			</div>
+
+			<DropdownMenu>
+				<DropdownMenuTrigger as-child>
+					<Button variant="outline" class="ml-auto"> Columns <ChevronDown class="ml-2 h-4 w-4" /> </Button>
+				</DropdownMenuTrigger>
+
+				<DropdownMenuContent align="end">
+					<DropdownMenuCheckboxItem
+						v-for="column in table.getAllColumns().filter((column) => column.getCanHide())"
+						:key="column.id"
+						class="capitalize"
+						:modelValue="column.getIsVisible()"
+						@update:modelValue="
+							(value: any) => {
+								column.toggleVisibility(!!value);
+							}
+						"
+					>
+						{{ column.id }}
+					</DropdownMenuCheckboxItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</div>
+
+		<div class="rounded-md border">
 
 			<Table>
 				<TableHeader>
